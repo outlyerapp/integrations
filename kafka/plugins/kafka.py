c145719c1f49b54e6cd05cc25b589f62708b8655
Oliver Greenaway <oliver.greenaway@outlyer.com>
2017-11-14 14:12:00 +0000

    dots, not underscores

--- kafka/plugins/kafka.py
+++ kafka/plugins/kafka.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 from outlyer_agent.collection import Status, Plugin, PluginTarget
 from outlyer_agent.java import MetricType
@@ -11,82 +12,82 @@ class KafkaPlugin(Plugin):
         time_now = time.monotonic()
 
         host = target.get('host', 'localhost')
-        port = target.get('port', 55555)
+        port = target.get('port', 9999)
         jmx_url = f'service:jmx:rmi:///jndi/rmi://{host}:{port}/jmxrmi'
 
         response = JvmTask().get_metrics(
             jmx_url,
 
             # Controller metrics
-            JmxQuery('kafka_controller_{name}_{attr}',
+            JmxQuery('kafka.controller.{name}.{attr}',
                      MetricType.GAUGE,
                      'kafka.controller:type=KafkaController,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_controller_stats_{name}',
+            JmxQuery('kafka.controller.stats.{name}',
                      MetricType.COUNTER,
                      'kafka.controller:type=ControllerStats,*',
                      JmxAttribute('Count')),
 
             # Network metrics
-            JmxQuery('kafka_network_processor_idle_percent_{networkProcessor}',
+            JmxQuery('kafka.network.processor.idle.percent.{networkProcessor}',
                      MetricType.GAUGE,
                      'kafka.network:type=Processor,name=IdlePercent,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_network_processor_avg_idle_percent',
+            JmxQuery('kafka.network.processor.avg.idle.percent',
                      MetricType.GAUGE,
                      'kafka.network:type=SocketServer,name=NetworkProcessorAvgIdlePercent',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_network_request_metrics_{name}_{request}',
+            JmxQuery('kafka.network.request.metrics.{name}.{request}',
                      MetricType.COUNTER,
                      'kafka.network:type=RequestMetrics,*',
                      JmxAttribute('Count')),
-            JmxQuery('kafka_network_request_channel_{name}',
+            JmxQuery('kafka.network.request.channel.{name}',
                      MetricType.GAUGE,
                      'kafka.network:type=RequestChannel,name=RequestQueueSize,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_network_request_channel_{name}_{processor}',
+            JmxQuery('kafka.network.request.channel.{name}.{processor}',
                      MetricType.GAUGE,
                      'kafka.network:type=RequestChannel,name=ResponseQueueSize,*',
                      JmxAttribute('Value')),
 
             # Server metrics
-            JmxQuery('kafka_server_broker_state',
+            JmxQuery('kafka.server.broker.state',
                      MetricType.GAUGE,
                      'kafka.server:type=KafkaServer,name=BrokerState',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_purgatory_num_delayed_{delayedOperation}',
+            JmxQuery('kafka.server.purgatory.num.delayed.{delayedOperation}',
                      MetricType.COUNTER,
                      'kafka.server:type=DelayedOperationPurgatory,name=NumDelayedOperations,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_purgatory_size_{delayedOperation}',
+            JmxQuery('kafka.server.purgatory.size.{delayedOperation}',
                      MetricType.GAUGE,
                      'kafka.server:type=DelayedOperationPurgatory,name=PurgatorySize,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_broker_topic_metrics_{name}',
+            JmxQuery('kafka.server.broker.topic.metrics.{name}',
                      MetricType.COUNTER,
                      'kafka.server:type=BrokerTopicMetrics,*',
                      JmxAttribute('Count')),
-            JmxQuery('kafka_server_delayed_fetch_{fetcherType}_{name}',
+            JmxQuery('kafka.server.delayed.fetch.{fetcherType}.{name}',
                      MetricType.COUNTER,
                      'kafka.server:type=DelayedFetchMetrics,*',
                      JmxAttribute('Count')),
-            JmxQuery('kafka_server_replica_fetcher_manager_{name}_{clientId}',
+            JmxQuery('kafka.server.replica.fetcher.manager.{name}.{clientId}',
                      MetricType.GAUGE,
                      'kafka.server:type=ReplicaFetcherManager,*',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_replica_manager_{name}',
+            JmxQuery('kafka.server.replica.manager.{name}',
                      MetricType.COUNTER,
                      'kafka.server:type=ReplicaManager,name=*PerSec',
                      JmxAttribute('Count')),
-            JmxQuery('kafka_server_replica_manager_{name}',
+            JmxQuery('kafka.server.replica.manager.{name}',
                      MetricType.GAUGE,
                      'kafka.server:type=ReplicaManager,name=*Count',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_replica_manager_{name}',
+            JmxQuery('kafka.server.replica.manager.{name}',
                      MetricType.GAUGE,
                      'kafka.server:type=ReplicaManager,name=UnderReplicatedPartitions',
                      JmxAttribute('Value')),
-            JmxQuery('kafka_server_request_handler_pool_avg_idle_percent',
+            JmxQuery('kafka.server.request.handler.pool.avg.idle.percent',
                      MetricType.COUNTER,
                      'kafka.server:type=KafkaRequestHandlerPool,name=RequestHandlerAvgIdlePercent',
                      JmxAttribute('Count')),
