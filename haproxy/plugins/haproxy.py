3846ed595afa0ae9398b49849dd59833b9df4da2
Todd Radel <todd@radel.us>
2017-11-27 22:24:23 -0500

    Add support for stats via HTTP

--- haproxy/plugins/haproxy.py
+++ haproxy/plugins/haproxy.py
@@ -1,33 +1,76 @@
 import csv
 import io
 import socket
+import requests
 
-from outlyer_agent.collection import Status, Plugin, PluginTarget, DEFAULT_PLUGIN_EXEC, Metric
-
+from outlyer_agent.collection import Status, Plugin, PluginTarget
 
 class HAProxyPlugin(Plugin):
 
-    def collect(self, target: PluginTarget) -> Status:
+    STATUS_MAP = {'UP': 0, 'DOWN': 1}
 
-        def status_code(status):
-            if status == 'UP':
-                return 0
-            elif status == 'DOWN':
-                return 1
-            else:
-                return 2
+    COUNTER_METRICS = [
+        'stot', 'bin', 'bout', 'dreq', 'dresp', 'ereq', 'econ', 'eresp',
+        'wretr', 'wredis', 'chkfail', 'chkdown', 'downtime', 'lbtot',
+        'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'hrsp_other',
+        'hanafail', 'req_tot', 'cli_abrt', 'srv_abrt',
+        'comp_in', 'comp_out', 'comp_byp', 'comp_rsp'
+    ]
 
-        sock_path = target.get('haproxy_sock', '/var/run/haproxy.sock')
+    GAUGE_METRICS = [
+        'qcur', 'qmax', 'scur', 'smax', 'slim', 'status', 'weight', 'act', 'bck', 'lastchg',
+        'qlimit', 'throttle', 'type', 'rate', 'rate_lim', 'rate_max', 'req_rate', 'req_rate_max',
+        'lastsess', 'qtime', 'ctime', 'rtime', 'ttime'
+    ]
 
+    def connect_socket(self, path):
+        in_buf = io.StringIO()
         try:
             s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            s.connect(sock_path)
-            s.send(b'show stat \n')
-            data = s.recv(32768)
+            s.connect(path)
+            s.send(b'show stat\n')
+            while True:
+                data = s.recv(4096)
+                in_buf.write(data)
+                if not data:
+                    break
             s.close()
         except OSError as ex:
-            self.logger.error('Unable to open %s: %s', sock_path, str(ex))
-            return Status.UNKNOWN
+            self.logger.error('Unable to open %s: %s', path, str(ex))
+        finally:
+            return in_buf.getvalue()
+
+    def connect_http(self, url, username=None, password=None):
+        r = requests.get(url, auth=(username, password))
+        return r.content
+
+
+    def translate_status(self, status):
+        return self.STATUS_MAP.get(status, 2)
+
+    def collect(self, target: PluginTarget) -> Status:
+
+        data = None
+        if target.get('mode', 'socket') == 'socket':
+            sock_path = target.get('haproxy_sock', '/var/run/haproxy.sock')
+            data = self.connect_socket(sock_path)
+        else:
+            url = target.get('url')
+            username = target.get('username')
+            password = target.get('password')
+
+            if not url:
+                self.logger.error('HTTP mode specified but no URL provided')
+                return Status.UNKNOWN
+
+            if ';csv' not in url:
+                url += ';csv;norefresh'
+
+            response = self.connect_http(url, username, password)
+            data = response.content
+
+        if data is None:
+            return Status.CRITICAL
 
         in_buf = io.StringIO(data.decode('utf-8'))
 
@@ -36,11 +79,24 @@ class HAProxyPlugin(Plugin):
             row.pop('')
             pxname = row.pop('# pxname')
             svname = row.pop('svname')
-            row['status'] = status_code(row['status'])
-            for k, v in row.items():
+            row['status'] = self.translate_status(row['status'])
+
+            labels = {'pxname': pxname, 'svname': svname}
+            for k in self.COUNTER_METRICS:
                 try:
-                    v = float(v)
-                    target.gauge(f'haproxy_{pxname}_{svname}_k').set(v)
+                    val = float(row[k])
+                    target.counter(f'haproxy.{k}', labels).set(val)
+                except KeyError:
+                    pass
+                except ValueError:
+                    pass
+
+            for k in self.GAUGE_METRICS:
+                try:
+                    val = float(row[k])
+                    target.gauge(f'haproxy.{k}', labels).set(val)
+                except KeyError:
+                    pass
                 except ValueError:
                     pass
 
