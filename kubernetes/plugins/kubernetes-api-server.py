519f6f5cfc35c11df1686d4f16704a2c5fdd5964
Jorge Acetozi <jorge.acetozi@gmail.com>
2018-05-23 14:13:27 -0300

    Scrapes specific API Server metrics and convert them from Prometheus to Native format

--- kubernetes/plugins/kubernetes-api-server.py
+++ kubernetes/plugins/kubernetes-api-server.py
@@ -7,39 +7,68 @@ client to enable Authentication with the server
 
 import sys
 import os
-
+from outlyer_plugin import Plugin, Status
+from prometheus_client.parser import text_string_to_metric_families
 from kubernetes import client, config
 
-# Get endpoint, defaults to healthz
-endpoint = '/' + os.environ['endpoint'] if 'endpoint' in os.environ else '/healthz'
+GAUGE_METRICS = [
+    'apiserver_request_latencies_bucket',
+]
 
-config.load_incluster_config()
-v1 = client.CoreV1Api()
-# Authentication setting
-auth_settings = ['BearerToken']
+COUNTER_METRICS = [
+    'apiserver_request_count',
+]
 
-if 'healthz' in endpoint:
-    try:
-        res = v1.api_client.call_api(endpoint, 'GET',
-                                     auth_settings=auth_settings,
-                                     _request_timeout=20,
-                                     response_type=str)
-        if res[1] == 200:
-            sys.exit(0)
+
+class KubeApiServerPlugin(Plugin):
+
+    def collect(self, _):
+        config.load_incluster_config()
+        v1 = client.CoreV1Api()
+
+        # Authentication setting
+        auth_settings = ['BearerToken']
+
+        # Get endpoint, defaults to healthz
+        endpoint = '/' + os.environ['endpoint'] if 'endpoint' in os.environ else '/healthz'
+
+        if 'healthz' in endpoint:
+            try:
+                res = v1.api_client.call_api(endpoint, 'GET',
+                                             auth_settings=auth_settings,
+                                             _request_timeout=20,
+                                             response_type=str)
+                if res[1] == 200:
+                    return Status.OK
+                else:
+                    self.logger.error('Expected response code 200 but got: %s', res[1])
+                    return Status.CRITICAL
+            except Exception as ex:
+                self.logger.error('Unable to reach %s: %s', endpoint, str(ex))
+                return Status.CRITICAL
         else:
-            sys.exit(2)
-    except Exception as err:
-        sys.exit(2)
-else:
-    try:
-        # Scrape a Promtheus endpoint
-        res = v1.api_client.call_api(endpoint, 'GET',
-                                     auth_settings=auth_settings,
-                                     _request_timeout=20,
-                                     _return_http_data_only=True,
-                                     response_type=str)
+            try:
+                # Scrape a Promtheus endpoint
+                res = v1.api_client.call_api(endpoint, 'GET',
+                                             auth_settings=auth_settings,
+                                             _request_timeout=20,
+                                             _return_http_data_only=True,
+                                             response_type=str)
 
-        print(res)
-        sys.exit(0)
-    except Exception:
-        sys.exit(2)
+                # Covert Prometheus response to Outlyer Native format
+                for family in text_string_to_metric_families(res):
+                    for sample in family.samples:
+                        labels = {k: v for k, v in sample[1].items() if v != ''}
+                        if sample[0] in COUNTER_METRICS:
+                            self.counter(sample[0], labels).set(sample[2])
+                        elif sample[0] in GAUGE_METRICS:
+                            self.gauge(sample[0], labels).set(sample[2])
+
+                return Status.OK
+            except Exception as ex:
+                self.logger.error('API Server is unavailable: %s', str(ex))
+                return Status.CRITICAL
+
+
+if __name__ == '__main__':
+    sys.exit(KubeApiServerPlugin().run())
