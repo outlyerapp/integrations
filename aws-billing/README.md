ba56355bc0ea241fab5f36e73384ef73a8be9b1f
dgildeh <dgildeh@davidgildeh.com>
2018-05-24 15:48:21 -0700

    Added AWS Billing Integration

---
+++ aws-billing/README.md
@@ -0,0 +1,49 @@
+AWS Billing Integration
+=======================
+
+== Description ==
+
+This integration pulls your estimated billing costs from AWS Cloudwatch so you can easily keep an eye on your
+AWS costs for each service and linked account from within Outlyer.
+
+== Metrics Collected ==
+
+| Metric Name                 |Type |Labels                              |Unit |Description                                                           |
+|-----------------------------|-----|------------------------------------|-----|----------------------------------------------------------------------|
+|aws.billing_estimated_charges|Gauge|servicename, currency, linkedaccount|Count|Total estimated charges per service/account for current billing period|
+
+== Installation ==
+
+To get started please ensure that your billing preferences have been updated to send billing metrics to
+Cloudwatch so these metrics can be queried with this integration. You can do this easily from the
+[AWS Billing Preferences Page](https://console.aws.amazon.com/billing/home?region=us-east-1#/preference)
+(ensure you have permissions to access this page) and enabling the `Enable Billing Alerts` checkbox.
+
+In order for this integration to run, you must create an IAM role and access keys for the plugin
+to connect to your AWS Cloudwatch metrics:
+
+1. To get started, open the AWS Management Console
+2. Click the IAM tab.
+3. Click the Create a New Group of Users button.
+4. Enter a Group Name called Outlyer.
+5. Select the Read Only Access Policy Template then click Continue.
+6. Click the Create New Users tab.
+7. Enter a new User Name called Outlyer and click Continue and then Finish.
+8. Click Show User Security Credentials.
+9. Copy and paste your Access Key Id and the Secret Access Key somewhere safe.
+
+The plugin must set the following variables to run:
+
+* `AWS_ACCESS_KEY_ID`: The AWS Access Key copied above
+* `AWS_SECRET_ACCESS_KEY`: The AWS Secret Key copied above
+
+In addition the following variables can be set to override the default values:
+
+* `time_range`: The time range in hours to query your metrics over. By default this is set to the last
+1 hour but if you run your functions rarely this can be set to longer for testing purposes.
+
+== Changelog ==
+
+|Version|Release Date|Description                                         |
+|-------|------------|----------------------------------------------------|
+|1.0    |24-May-2018 |Initial version of our AWS billing integration      |
---
+++ aws-billing/plugins/aws-billing.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import boto3
+import sys
+import uuid
+from datetime import datetime, timedelta
+
+from outlyer_plugin import Plugin, Target, Status
+
+
+class AWSBilling(Plugin):
+
+    def collect(self, _):
+        try:
+            time_range = self.get('time_range', '24')
+
+            # Create CloudWatch client for us-east-1 region
+            # All billing metrics are stored in this region
+            cloudwatch = boto3.client('cloudwatch', 'us-east-1')
+
+            # List all metrics through the pagination interface
+            metrics = {}
+            paginator = cloudwatch.get_paginator('list_metrics')
+            for response in paginator.paginate(Namespace='AWS/Billing'):
+                for metric in response['Metrics']:
+                    # Add a uuid so we can link metric values to original metric
+                    id = 'a_' + uuid.uuid4().hex
+                    metrics[id] = metric
+
+            # Build metric query to get metric values
+            query = []
+            for key, value in metrics.items():
+                query.append({
+                    'Id': key,
+                    'MetricStat': {
+                        'Metric': {
+                            'Namespace': value['Namespace'],
+                            'MetricName': value['MetricName'],
+                            'Dimensions': value['Dimensions']
+                        },
+                        'Period': 300,
+                        'Stat': 'Maximum',
+                    }
+                })
+
+            # Get metric values
+            end_time = datetime.utcnow()
+            start_time = end_time - timedelta(hours=int(time_range))
+            values = cloudwatch.get_metric_data(
+                MetricDataQueries=query,
+                StartTime=start_time,
+                EndTime=end_time,
+                ScanBy='TimestampAscending')
+
+            for value in values['MetricDataResults']:
+                metric = metrics[value['Id']]
+                metric_labels = self.convertDimensionToDict(metric['Dimensions'])
+                metric_value = 0
+                if len(value['Values']) > 0:
+                    metric_value = value['Values'][-1]
+                self.gauge('aws.billing_estimated_charges', metric_labels).set(metric_value)
+
+            return Status.OK
+
+        except Exception as err:
+            raise err
+            return Status.UNKNOWN
+
+    def convertDimensionToDict(self, dimensions):
+        labels = {}
+        for label in dimensions:
+            labels[label['Name']] = label['Value']
+        return labels
+
+
+if __name__ == '__main__':
+    sys.exit(AWSBilling().run())
