7e35e209589575103ccf3da0ada45dd2e30cb9f4
dgildeh <dgildeh@davidgildeh.com>
2018-06-19 15:53:00 +0100

    Updated kube state metrics plugin

--- kubernetes/plugins/kube-state-metrics.py
+++ kubernetes/plugins/kube-state-metrics.py
@@ -2,11 +2,9 @@
 
 import sys
 import requests
-import os
 
 from outlyer_plugin import Plugin, Status
 from prometheus_client.parser import text_string_to_metric_families
-
 
 GAUGE_METRICS = [
     'kube_node_status_condition',
@@ -32,26 +30,26 @@ COUNTER_METRICS = [
 
 
 class KubeStateMetricsPlugin(Plugin):
-  
-  def collect(self, _):
-    HOST = os.environ['KSM_HOST'] if 'KSM_HOST' in os.environ else 'kube-state-metrics.kube-system'
-    PORT = os.environ['KSM_PORT'] if 'KSM_PORT' in os.environ else '8080'
-    PATH = os.environ['KSM_PATH'] if 'KSM_PATH' in os.environ else 'metrics'
-    
-    try:
-      res = requests.get(f'http://{HOST}:{PORT}/{PATH}', timeout=20).text
 
-      for family in text_string_to_metric_families(res):
-        for sample in family.samples:
-          if sample[0] in COUNTER_METRICS:
-            self.counter(sample[0], sample[1]).set(sample[2])
-          elif sample[0] in GAUGE_METRICS:
-            self.gauge(sample[0], sample[1]).set(sample[2])
-      
-      return Status.OK
-    except Exception as ex:
-      self.logger.error('Unable to scrape metrics from kube-state-metrics: %s', str(ex))
-      return Status.CRITICAL
+    def collect(self, _):
+        HOST = self.get('ip', 'kube-state-metrics.kube-system')
+        PORT = self.get('port', '8080')
+        PATH = self.get('endpoint', 'metrics')
+
+        try:
+            res = requests.get(f'http://{HOST}:{PORT}/{PATH}', timeout=20).text
+
+            for family in text_string_to_metric_families(res):
+                for sample in family.samples:
+                    if sample[0] in COUNTER_METRICS:
+                        self.counter(sample[0], sample[1]).set(sample[2])
+                    elif sample[0] in GAUGE_METRICS:
+                        self.gauge(sample[0], sample[1]).set(sample[2])
+
+            return Status.OK
+        except Exception as ex:
+            self.logger.error('Unable to scrape metrics from kube-state-metrics: %s', str(ex))
+            return Status.CRITICAL
 
 
 if __name__ == '__main__':
