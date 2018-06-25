568b66128d55dc3ca0620c797a8d74a3b804fbd6
dgildeh <dgildeh@davidgildeh.com>
2018-06-25 10:25:13 +0100

    Added ec2 folder and discovery script

---
+++ aws-ec2/README.md
@@ -0,0 +1,15 @@
+AWS EC2 Integration
+===================
+
+== Description ==
+
+== Metrics Collected ==
+
+== Installation ==
+
+
+== Changelog ==
+
+|Version|Release Date|Description                                          |
+|-------|------------|-----------------------------------------------------|
+|1.0    |25-Jun-2018 |Initial version of our EC2 monitoring integration.   |
---
+++ aws-ec2/plugins/ec2-discovery.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import boto3
+import sys
+import os
+import json
+
+class EC2Discovery(object):
+
+    def discover(self):
+        try:
+            instances = []
+
+            aws_region = os.environ.get('AWS_REGION')
+            if not aws_region:
+                raise Exception("Please ensure AWS_REGION is set.")
+            awsclient = boto3.client('ec2',
+                                     region_name=aws_region,
+                                     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+                                     aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
+            paginator = awsclient.get_paginator('describe_instances')
+            for response in paginator.paginate():
+                for reservation in response['Reservations']:
+                    for instance in reservation['Instances']:
+
+                        if instance['State']['Name'] == 'running':
+
+                            host = {
+                                'hostname': "ec2-" + instance['PrivateDnsName'],
+                                'ip': instance['PrivateIpAddress'],
+                            }
+                            labels = {
+                                'instance.type': 'host',
+                                'cloud.provider': 'aws',
+                                'cloud.service': 'aws.ec2',
+                                'cloud.instance.region': aws_region,
+                                'cloud.instance.az': instance['Placement']['AvailabilityZone'],
+                                'cloud.instance.id': instance['InstanceId'],
+                                'cloud.instance.type': instance['InstanceType'],
+                                'cloud.instance.image_id': instance['ImageId']
+                            }
+
+                            if 'Tags' in instance:
+                                for tag in instance['Tags']:
+                                    key = tag['Key']
+                                    value = tag['Value']
+                                    labels[key] = value
+
+                            host['labels'] = labels
+                            instances.append(host)
+
+            # Output instance in JSON to Stdout
+            print(json.dumps({"instances": instances, "version": "0.1.0"}))
+            return 0
+
+        except Exception as err:
+            raise err
+            return 3
+
+if __name__ == '__main__':
+    sys.exit(EC2Discovery().discover())
