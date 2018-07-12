e4bf43cf528b19d80493cdf1780417be33d4416a
David Gildeh <dgildeh@davidgildeh.com>
2018-07-12 15:56:12 -0700

    Initialised ruby project files

---
+++ sensu/Dev-Instructions.md
@@ -0,0 +1,3 @@
+# Sensu Outlyer Handler Plugin
+
+This readme includes development instructions for building, packaging and testing the Sensu plugin in Ruby.
---
+++ sensu/lib/sensu-plugins-outlyer/version.rb
@@ -0,0 +1,9 @@
+module SensuPluginsOutlyer
+  module Version
+    MAJOR = 1
+    MINOR = 2
+    PATCH = 0
+
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+  end
+end
